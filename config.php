f77d64515676a82a349f8d0fd306db8b3c500423
Mihail B <1mihails.b@gmail.com>
2020-08-23 16:30:11 +0300

    fix typoe

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('D_S', DIRECTORY_SEPARATOR);
 
 // in $folders array put all the folders in path from (excluding) root directory to the project folder
 // for example, on windows OS, where root folder is C:\Users\CurrentUser and full path to the project is
-// C:\Users\CurrentUser\Desktop\scandi\php, add all folders after the "CurrentUser' like I do on line 13
+// C:\Users\CurrentUser\Desktop\scandi\php, add all folders after the "CurrentUser' like I do on line 10
 // with this setup, in browser I use the url http://localhost:8080/Desktop/scandi/php/
 $folders = array('Desktop', 'scandi', 'php');
 $config['base_url'] = D_S . implode(D_S, $folders);
