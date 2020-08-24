5fe345878b46470478e58e4f3ddc0b51aea50c68
Mihail B <1mihails.b@gmail.com>
2020-08-24 11:57:16 +0300

    fix description

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ define('D_S', DIRECTORY_SEPARATOR);
 
 // in $folders array put all the folders in path from (excluding) root directory to the project folder
 // for example, on windows OS, where root folder is C:\Users\CurrentUser and full path to the project is
-// C:\Users\CurrentUser\Desktop\scandi\php, add all folders after the "CurrentUser' like I do on line 10
-// with this setup, in browser I use the url http://localhost:8080/Desktop/scandi/php/
+// C:\Users\CurrentUser\Desktop\php, add all folders after the "CurrentUser' like I do on line 10
+// with this setup, in browser I use the url http://localhost:8080/Desktop/php/
 $folders = array('Desktop', 'php');
 $config['base_url'] = D_S . implode(D_S, $folders);
 
